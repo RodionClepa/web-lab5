72fa1d80e4c721f8babdcd6292fe34c699f97512
RodionClepa <raduclepa@gmail.com>
2025-03-18 09:44:29 +0200

    <feat>[url-command]<Add Parser url>

--- go2web.py
+++ go2web.py
@@ -1,5 +1,20 @@
 import sys
 
+def parse_url(url):
+    if not url.startswith("http://") and not url.startswith("https://"):
+        url = "http://" + url
+    protocol, rest = url.split("//", 1)
+    host, _, path = rest.partition("/")
+    path = "/" + path if path else "/"
+    return protocol, host, path
+
+def fetch_url(url):
+    protocol, host, path = parse_url(url)
+    print(protocol)
+    print(host)
+    print(path)
+    
+
 def main():
     if len(sys.argv) < 2:
         print("to low arguments, calling help")
@@ -10,7 +25,7 @@ def main():
     if option == "-h":
         print("show_help()")
     elif option == "-u" and len(sys.argv) > 2:
-        print("fetch_url(sys.argv[2])")
+        fetch_url(sys.argv[2])
     elif option == "-s" and len(sys.argv) > 2:
         print("search_query(sys.argv[2])")
     else:
