30e319ca268bba386474b4f0fbcabe75f436d524
Rodion <93823382+RodionClepa@users.noreply.github.com>
2025-03-18 09:59:26 +0200

    Feature/url command (#2)
    * <feat>[url-command]<Add Parser url>
    
    * <feat>[url-command]<Add http request function>
    
    * <feat>[url-command]<Add extractor body>

--- go2web.py
+++ go2web.py
@@ -1,4 +1,47 @@
 import sys
+import socket
+import ssl
+import re
+
+def parse_url(url):
+    if not url.startswith("http://") and not url.startswith("https://"):
+        url = "http://" + url
+    protocol, rest = url.split("//", 1)
+    host, _, path = rest.partition("/")
+    path = "/" + path if path else "/"
+    return protocol, host, path
+
+def make_http_request(host, path, use_ssl=True):
+    port = 443 if use_ssl else 80
+    request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nUser-Agent: go2web-cli\r\nConnection: close\r\n\r\n"
+    
+    sock = socket.create_connection((host, port))
+    if use_ssl:
+        context = ssl.create_default_context()
+        sock = context.wrap_socket(sock, server_hostname=host)
+    
+    sock.sendall(request.encode())
+    response = b""
+    while True:
+        data = sock.recv(4096)
+        if not data:
+            break
+        response += data
+    sock.close()
+    
+    return response.decode(errors="ignore")
+
+def extract_text(html):
+    text = re.sub(r'<[^>]+>', '', html)  # Remove HTML tags
+    text = re.sub(r'\s+', ' ', text)  # Normalize whitespace
+    return text.strip()
+
+def fetch_url(url):
+    protocol, host, path = parse_url(url)
+    response = make_http_request(host, path, use_ssl=(protocol == "https:"))
+    headers, body = response.split("\r\n\r\n", 1)
+    print(extract_text(body))
+    
 
 def main():
     if len(sys.argv) < 2:
@@ -10,7 +53,7 @@ def main():
     if option == "-h":
         print("show_help()")
     elif option == "-u" and len(sys.argv) > 2:
-        print("fetch_url(sys.argv[2])")
+        fetch_url(sys.argv[2])
     elif option == "-s" and len(sys.argv) > 2:
         print("search_query(sys.argv[2])")
     else:
