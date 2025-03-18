edfe654fc3d31b8e782c5c52a4d19469cf0338f6
Rodion <93823382+RodionClepa@users.noreply.github.com>
2025-03-18 10:11:42 +0200

    Feature/term command (#3)
    * <feat>[term-command]<Add links getter>
    
    * <feat>[term-command]<Add links cleaner>

--- go2web.py
+++ go2web.py
@@ -2,6 +2,7 @@ import sys
 import socket
 import ssl
 import re
+import urllib.parse
 
 def parse_url(url):
     if not url.startswith("http://") and not url.startswith("https://"):
@@ -41,7 +42,23 @@ def fetch_url(url):
     response = make_http_request(host, path, use_ssl=(protocol == "https:"))
     headers, body = response.split("\r\n\r\n", 1)
     print(extract_text(body))
-    
+
+def clean_duckduckgo_link(link):
+    match = re.search(r'uddg=([^&]+)', link)
+    if match:
+        return urllib.parse.unquote(match.group(1))
+    return link
+
+def search_query(term):
+    query = term.replace(" ", "+")
+    protocol, host, path = parse_url(f"https://html.duckduckgo.com/html/?q={query}")
+    response = make_http_request(host, path)
+    links = re.findall(r'<a rel="nofollow" class="result__a" href="(.*?)">(.*?)</a>', response)
+    print(links)
+
+    for i, (link, title) in enumerate(links[:10], 1):
+        cleaned_link = clean_duckduckgo_link(link)
+        print(f"{i}. {extract_text(title)} - {cleaned_link}")
 
 def main():
     if len(sys.argv) < 2:
@@ -55,7 +72,7 @@ def main():
     elif option == "-u" and len(sys.argv) > 2:
         fetch_url(sys.argv[2])
     elif option == "-s" and len(sys.argv) > 2:
-        print("search_query(sys.argv[2])")
+        search_query(sys.argv[2])
     else:
         print("Invalid command. Use -h for help.")    
 
